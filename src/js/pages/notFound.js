10027967210a9f287918b956a96599614ca3daa7
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-04-02 20:42:45 -0300

    FEAT: adição de estilização a page not found

--- src/js/pages/notFound.js
+++ src/js/pages/notFound.js
@@ -1,8 +1,8 @@
 const notFound = `
-    <section class="p-3 w-100">
+    <section class="p-3 w-100 d-flex flex-column justify-content-center align-items-center">
         <h1>404 - Página Não Encontrada</h1>
         <p>A página que você está tentando acessar não existe. Verifique a URL ou volte para a página inicial.</p>
-        <a href="/" style="color: blue; text-decoration: underline;">Voltar para a Home</a>
+        <a class="btn btn-dark p-3" href="/">Voltar para o Início</a>
     </section>
 `;
 
