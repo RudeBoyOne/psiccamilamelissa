f57042fe31eca4b5575adfae72a16ff8142f717e
Lucas Ferreira Nogueira <lucas.ferreiranogueira@outlook.com>
2025-04-02 12:30:07 -0300

    FEAT: adição de schema.org e google analytics

--- src/js/pages/home.js
+++ src/js/pages/home.js
@@ -4,21 +4,21 @@ import '../../css/pages/home.scss'
 const homePage = `
     <section class="d-flex flex-column-reverse flex-lg-row align-items-center p-3">
         <section class="col-12 col-lg-6 pt-4 p-lg-4 d-flex flex-column justify-content-evenly text-section">
-            <h1 class="fw-bold fs-6 text-decoration-underline text-uppercase">  Atendimento clínico de qualidade </h1>
+            <h1 class="fw-bold text-uppercase">  Atendimento clínico </h1>
             <p class="text-justify lh-lg">
                 focado na abordagem psicanalítica. A psicanálise é uma ferramenta poderosa para explorar o inconsciente e entender os fatores que influenciam nossos comportamentos e emoções. Meu objetivo é ajudar você a enfrentar desafios e compreender suas emoções.
             </p>
-            <h2 class="fw-bold fs-6 fs-sm-2 text-decoration-underline text-uppercase"> Por que escolher a psicanálise? </h2>
+            <h2 class="fw-bold fs-6 text-uppercase"> Por que escolher a psicanálise? </h2>
             <p class="text-justify lh-lg">
                 A psicanálise oferece um espaço para mergulhar profundamente em si mesmo, revelando aspectos ocultos que influenciam seu bem-estar emocional. Compreender o inconsciente pode ser transformador e libertador, permitindo uma vida mais consciente.
             </p>
-            <h2 class="fw-bold fs-6 fs-sm-2 text-decoration-underline text-uppercase"> Na minha clínica </h2>
+            <h2 class="fw-bold fs-6 text-uppercase"> Na minha clínica </h2>
             <p class="text-justify lh-lg">
                 você encontrará um ambiente acolhedor e seguro, onde suas histórias, dores e alegrias serão respeitadas e valorizadas. Acredito que cada indivíduo é único e merece uma escuta atenta e sensível, para que possamos juntos ressignificar experiências e promover o autoconhecimento.
             </p>
-            <p class="text-justify fw-bold text-uppercase">
+            <h6 class="text-justify fw-bold text-uppercase">
                 Venha conhecer meu trabalho e vamos juntos trilhar um caminho de autoconhecimento e transformação.
-            </p>
+            </h6>
 
             <div id="container" class="d-flex justify-content-center button-home">
                 <a id="whatsapp-link" href="https://wa.me/5511959525369?text=Olá!%20Gostaria%20de%20mais%20informações%20sobre%20terapia/análise." target="_blank" class="learn-more">
